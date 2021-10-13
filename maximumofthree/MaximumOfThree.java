7c4a2eb5e02d79dacf59317927bebc8bfcb8b48d
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-13 14:39:15 +0530

    Added: Program to find maximum using sorting

--- maximumofthree/MaximumOfThree.java
+++ maximumofthree/MaximumOfThree.java
@@ -1,17 +1,36 @@
 package maximumofthree;
 
-public class MaximumOfThree {
-	public static String maxOfThree(String x, String y, String z) {
-		String max = x;
-		if (y.compareTo(max) > 0)
-			max = y;
-		if (z.compareTo(max) > 0)
-			max = z;
-		return max;
+public class MaximumOfThree<T extends Comparable<T>> {
+	T[] array;
+
+	public MaximumOfThree(T[] array) {
+			this.array = array;
+		}
+
+	public T testMaximum() {
+		return MaximumOfThree.testMaximum(array);
 	}
 
+	public static <T extends Comparable<T>> T testMaximum(T[] array) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = i; j < 4; j++) {
+				if (array[i].compareTo(array[j]) < 0) {
+					T a = array[i];
+					array[i] = array[j];
+					array[j] = a;
+				}
+			}
+		}
+		System.out.println("Maximum is :"+ array[0]);
+		return array[0];
+	}
+	
 	public static void main(String[] args) {
-		String maximum = MaximumOfThree.maxOfThree("Apple", "Banana", "Peach");
-		System.out.println("Maximum of three Strings is :" + maximum);
+		Integer[] arrInt = {12,43,5,63};
+		Float[] arrFloat = {1.2f,445.6f,87.4f,21.3f};
+		String[] arrString = {"Apple","Banana","Peach","Carrot"};
+		new MaximumOfThree<Integer>(arrInt).testMaximum();
+		new MaximumOfThree<Float>(arrFloat).testMaximum();
+		new MaximumOfThree<String>(arrString).testMaximum();
 	}
 }
